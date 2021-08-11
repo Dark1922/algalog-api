c95db8f12c33dce08a3c84167e14900ef9539f4d
dark1922 <jvdematos004@gmail.com>
2021-08-11 15:53:32 -0300

    feath: lombok , teste setando os dados do cliente

---
+++ algalog-api-1/src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -0,0 +1,34 @@
+package com.algaworks.algalog.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algalog.domain.model.Cliente;
+
+@RestController // declarar que é um controller pra funcionar os mapeamento
+public class ClienteController {
+
+	@GetMapping("/clientes") //mapeamento
+	public List<Cliente> listar() {
+		
+		Cliente cliente1 = new Cliente(); 
+		
+		cliente1.setId(1l);
+		cliente1.setNome("João");
+		cliente1.setEmail("jvdematos004@gmail.com");
+		cliente1.setTelefone("(61) 9 9423-0955");
+		
+		var cliente2 = new Cliente();
+		
+		cliente2.setId(2l);
+		cliente2.setNome("Paulo");
+		cliente2.setEmail("teste@gmail.com");
+		cliente2.setTelefone("(44) 9 9323-0942");
+		
+		return Arrays.asList(cliente1, cliente2);
+	}
+
+}
