6f2b7b1308bb73c61aab30f5a761845380a5da47
dark1922 <jvdematos004@gmail.com>
2021-08-12 19:14:25 -0300

    feath: crud com spring

--- algalog-api-1/src/main/java/com/algaworks/algalog/controller/ClienteController.java
+++ algalog-api-1/src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -2,26 +2,79 @@ package com.algaworks.algalog.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+//gera um construtor com todas propriedades que tem em nossa clase no caso clientrepository
+@AllArgsConstructor
+@RequestMapping("/clientes") // vai mapear automaticamente as rota como /clientes
 @RestController // declarar que é um controller pra funcionar os mapeamento
 public class ClienteController {
 
-	@PersistenceContext  //injeta uma variavel do maneger na variavel declarada
-	private EntityManager entityManager;
-	
-	@GetMapping("/clientes") //mapeamento
+	private ClienteRepository clienteRepository;
+
+	@GetMapping() // mapeamento /clientes
 	public List<Cliente> listar() {
-		
-		return entityManager.createQuery(" from Cliente ", Cliente.class)
-				.getResultList();
-		
+
+		return clienteRepository.findAll();
+
 	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
+
+		return clienteRepository.findById(id).map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+
+	}
+	@PostMapping //post pra criar user
+	@ResponseStatus(HttpStatus.CREATED)//status de criação qnd cria
+	//@RequestBody corpo da requisição transformar oq foi passado em um objeto java
+     public Cliente adicionar(@RequestBody Cliente cliente) {
+    	 
+    	 return clienteRepository.save(cliente);
+     } 
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id,
+			@RequestBody Cliente cliente) {
+		
+		//ta negando se o cliente não existe
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		cliente.setId(id); //atribuir o id ao objeto cliente pra força uma atualização 
+		
+		cliente = clienteRepository.save(cliente);
+		
+		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> remover(Long id) {
+		
+		//se o cliente que tá exluindo n existir notfound
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build(); //código 204 qnd n está voltando nada e exclui
+	}
 }
--- algalog-api-1/src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
+++ algalog-api-1/src/main/java/com/algaworks/algalog/domain/repository/ClienteRepository.java
@@ -1,9 +1,15 @@
 package com.algaworks.algalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algalog.domain.model.Cliente;
 
+@Repository  //repositorio que gerencia as entidades
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
+	List<Cliente> findByNome(String nome);//nome especifico
+	List<Cliente> findByNomeContaining(String nome); //usa o like procura por partes do nome
 }
